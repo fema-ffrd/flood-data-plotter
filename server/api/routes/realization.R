eb5513aa66bdac11d3ee5b355d469a5c83661451
Seth Lawler <slawler@dewberry.com>
2025-09-29 14:45:46 -0400

    add new endpoint (example commit)

--- server/api/routes/realization.R
+++ server/api/routes/realization.R
@@ -1,11 +1,18 @@
-# routes/flows.R
+# routes/realization
 library(plumber)
 library(jsonlite)
 
 source("../../src/plotting/realization_flow.R")
+source("../../src/plotting/realization_stage.R")
 
 #* Create Flow Plot in HTML or JSON
 #* @post /flows
 function(req, res) {
   create_plot_route(realization_flow_summary_plot, "realization_flow", req, res)
+}
+
+#* Create Stage Plot in HTML or JSON
+#* @post /stage
+function(req, res) {
+  create_plot_route(realization_flow_summary_plot, "realization_stage", req, res)
 }
